db25bcc59c9a4259e2a0cc43ecab5db91172bf56
JTTran <tranjt80@gmail.com>
2017-06-26 17:26:01 +0300

    Add project page

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render("maintenance.hbs");
-});
+// app.use((req, res, next) => {
+//     res.render("maintenance.hbs");
+// });
 
 app.use(express.static(__dirname + "/public"));
 
@@ -53,6 +53,13 @@ app.get("/bad", (req, res) => {
         errorMessage: "Unable to handle request"
     });
 }); 
+
+app.get("/projects", (req, res) => {
+    res.render("projects.hbs", {
+        pageTitle: "Project Page"        
+    });
+}); 
+
 
 
 app.listen(port, () => {
